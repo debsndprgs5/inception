4b9114db7be4ec09a7fcd308162d5bbbcf0fa74c
Zacchary Favere <zfavere@c1r4p6.42nice.fr>
2024-11-14 20:32:44 +0100

    quickadd

--- project/srcs/requirements/mariadb/tools/databasemaker.sh
+++ project/srcs/requirements/mariadb/tools/databasemaker.sh
@@ -40,8 +40,8 @@ EOF
     rm -f $tfile
 fi
 # Config mariadb to allow external connections & uncommenting skip-networking in mdb conf
-sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/mariadb.conf.d/
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/mariadb.conf.d/
+sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 # Start MariaDB
 service mysql start
