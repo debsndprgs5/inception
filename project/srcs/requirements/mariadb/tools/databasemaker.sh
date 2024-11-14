c434898005736b42898b839281fb1c5fbf766f2c
Zacchary Favere <zfavere@c1r4p6.42nice.fr>
2024-11-14 20:15:26 +0100

    quickadd

--- project/srcs/requirements/mariadb/tools/databasemaker.sh
+++ project/srcs/requirements/mariadb/tools/databasemaker.sh
@@ -44,4 +44,4 @@ sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
 # Start MariaDB
-exec /usr/bin/mysqld --user=mysql --console
+service mysql start
