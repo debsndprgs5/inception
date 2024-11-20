2c40c8f11602cf3c54df15d086bf6647f0508eb0
Zacchary Favere <zfavere@c1r4p8.42nice.fr>
2024-11-20 19:00:46 +0100

    quickadd

--- project/srcs/requirements/mariadb/tools/databasemaker.sh
+++ project/srcs/requirements/mariadb/tools/databasemaker.sh
@@ -1,47 +1,44 @@
-#!/bin/bash
+#!/bin/sh
 
-# Check if the directory doesn't exist
-if [ ! -d "/run/mysqld" ]; then
-    mkdir -p /run/mysqld
+mysql_install_db
 
-    # Set the owner and group for the directory
-    chown mysql:mysql /run/mysqld
+/etc/init.d/mysql start
 
-    # Set the permissions for the directory to be more restrictive
-    chmod 750 /run/mysqld
+#Check if the database exists
+
+if [ -d "/var/lib/mysql/mysql" ]
+then 
+
+    echo "Database already exists"
+else
+
+# Set root option so that connexion without root password is not possible
+
+mysql_secure_installation << _EOF_
+
+Y
+root4life
+root4life
+Y
+n
+Y
+Y
+_EOF_
+
+#Add a root user on 127.0.0.1 to allow remote connexion 
+#Flush privileges allow to your sql tables to be updated automatically when you modify it
+#mysql -uroot launch mysql command line client
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PWD'; FLUSH PRIVILEGES;" | mysql -uroot
+
+#Create database and user in the database for wordpress
+
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME; GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PWD'; FLUSH PRIVILEGES;" | mysql -u root
+
+#Import database in the mysql command line
+mysql -uroot -p$DB_ROOT_PWD $DB_NAME < /usr/local/bin/wordpress.sql
+
 fi
 
-# Initialize MySQL Data Directory if it hasn't been initialized yet
+/etc/init.d/mysql stop
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    chown -R mysql:mysql /var/lib/mysql
-    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
-    tfile=`mktemp`
-    if [ ! -f "$tfile" ]; then
-        return 1
-    fi
-    # Create and send init basic settings to a temp file, then used
-    # for a mdb bootstrap mode setup
-    cat << EOF > $tfile
-    USE mysql;
-    FLUSH PRIVILEGES;
-    DELETE FROM mysql.user WHERE User='';
-    DROP DATABASE test;
-    DELETE FROM mysql.db WHERE Db='test';
-    DELETE FROM mysql.user WHERE User='$DB_ROOT' AND Host NOT IN ('$DB_HOST', '127.0.0.1', '::1');
-    ALTER USER 'root'@'$DB_HOST' IDENTIFIED BY '$DB_ROOT_PWDD';
-    CREATE DATABASE $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
-    CREATE USER '$DB_USER'@'%' IDENTIFIED by '$DB_USER_PWD';
-    GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
-    GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'$DB_HOST' IDENTIFIED BY '$DB_USER_PWD';
-    FLUSH PRIVILEGES;
-EOF
-    /usr/bin/mysqld --user=mysql --bootstrap < $tfile
-    rm -f $tfile
-fi
-# Config mariadb to allow external connections & uncommenting skip-networking in mdb conf
-sed -i "s|skip-networking|# skip-networking|g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-# Start MariaDB
-#service mysql start
+exec "$@"
