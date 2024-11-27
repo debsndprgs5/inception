c7c0d907f88446c3adada02ef0ca4c955f5d0bf0
Zacchary Favere <zfavere@c1r4p1.42nice.fr>
2024-11-28 00:43:12 +0100

    quickadd

--- project/srcs/requirements/mariadb/tools/databasemaker.sh
+++ project/srcs/requirements/mariadb/tools/databasemaker.sh
@@ -15,7 +15,7 @@ else
 
     echo "CREATE DATABASE IF NOT EXISTS $DB_NAME" | mysql -u root
     echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PWD'" | mysql -u root
-    echo "GRANT ALL PRIVILEGES ON '$DB_NAME'.* TO '$DB_USER'@'%';" | mysql -u root
+    echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" | mysql -u root
     echo "FLUSH PRIVILEGES" | mysql -u root
     echo "ALTER USER '$DB_ROOT'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';" | mysql -u root --password=${DB_ROOT_PWD}
 #    echo "GRANT ALL ON *.* TO '$DB_ROOT'@'%' IDENTIFIED BY '$DB_ROOT_PWD';" | mysql -u root
