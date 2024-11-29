be9d6bb25c50965d04513b24b836011b694055a7
root <root@DESKTOP-0B914UH>
2024-11-29 22:18:28 +0100

    bloblote version

--- project/srcs/requirements/mariadb/tools/databasemaker.sh
+++ project/srcs/requirements/mariadb/tools/databasemaker.sh
@@ -1,36 +1,27 @@
 #!/bin/sh
 
-# Initialisation de MariaDB
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
-
-# Démarrer MariaDB
-mysqld_safe --datadir=/var/lib/mysql &
-sleep 5
-
 # Vérifier si la base de données existe
 if [ -d "/var/lib/mysql/$DB_NAME" ]; then
     echo "Database already exists"
 else
     echo "Setting up the database..."
 
-mysql_secure_installation << _EOF_
+service mysql start
+mysql -uroot<<EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PWD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+UPDATE mysql.user SET Password = PASSWORD('$DB_ROOT_PWD') WHERE User = 'root';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+FLUSH PRIVILEGES;
+QUIT
+EOF
 
-Y
-root4life
-root4life
-Y
-n
-Y
-Y
-_EOF_
-
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME" | mysql -u root
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PWD'" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" | mysql -u root
-echo "FLUSH PRIVILEGES" | mysql -u root
-echo "ALTER USER '$DB_ROOT'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';" | mysql -u root --password=${DB_ROOT_PWD}
 echo "Done setting up the db."
-
+sleep 5
+service mysql stop
 fi
-
-/etc/init.d/mysql stop
+sleep 5
+mysqld_safe --datadir=/var/lib/mysql
--- project/srcs/requirements/wordpress/tools/create_wp.sh
+++ project/srcs/requirements/wordpress/tools/create_wp.sh
@@ -2,26 +2,18 @@
 
 #Check if wp-config.php exists
 
+sleep 5
 if [ -f ./wp-config.php ]
 then
 	echo "wordpress already downloaded"
 else
-	#Download wordpress and all config file
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
-
-	#Inport env variables in the config file
-	sed -i "s/username_here/$DB_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$DB_USER_PWD/g" wp-config-sample.php
-	sed -i "s/localhost/$DB_HOST/g" wp-config-sample.php
-	sed -i "s/database_name_here/$DB_NAME/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
+	wp core download --allow-root
+	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_USER_PWD --dbhost=$DB_CONTAINER_NAME --force --allow-root
+	wp core install --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email
+	wp user create $WP_PHP_USER --allow-root $WP_USER_EMAIL --user_pass=$WP_USER_PWD
 fi
 
 rm -rf /etc/php/7.3/fpm/pool.d/www.conf
 envsubst < /etc/php/7.3/fpm/pool.d/www.conf.template > /etc/php/7.3/fpm/pool.d/www.conf
 
-exec "$@"
+/usr/sbin/php-fpm7.3 -F 
